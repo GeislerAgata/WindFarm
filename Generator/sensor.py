e18ff2b4670883c620af0bad30a3e98801a2b36f
Agata Geisler <geisler.agata@wp.pl>
2023-11-02 18:13:56 +0100

    Timestamp and unit added to the message

--- Generator/sensor.py
+++ Generator/sensor.py
@@ -1,18 +1,21 @@
+import json
 import sys
 from random import random
 from datetime import datetime
 from faker import Faker
 from faker.providers import date_time
+import random
 
 faker = Faker()
 faker.add_provider(date_time)
 
 
 class Sensor:
-    def __init__(self, sensor_id, sensor_type, queue):
+    def __init__(self, sensor_id, sensor_type, host):
         self.sensor_id = sensor_id
         self.sensor_type = sensor_type
-        self.queue = queue
+        self.host = host
+        self.unit = ""
 
         self.wind_speed_range = (3.0, 25.0)
         self.temperature_range = (-40.0, 50.0)
@@ -32,14 +35,18 @@ class Sensor:
 
         value = 0.0
         match self.sensor_type:
-            case "Wind speed":
+            case "wind_speed":
                 value = self.random_data(self.wind_speed_range)
-            case "Temperature":
+                self.unit = self.meters_per_second
+            case "temperature":
                 value = self.random_data(self.temperature_range)
-            case "Vibrations":
+                self.unit = self.celsius
+            case "vibrations":
                 value = self.random_data(self.vibrations_range)
-            case "Noise":
+                self.unit = self.millimeters
+            case "noise":
                 value = self.random_data(self.noise_range)
+                self.unit = self.decibels
             case _:
                 print("Error: Undefined sensor type")
                 sys.exit()
@@ -47,13 +54,20 @@ class Sensor:
 
     def generate_message(self):
         timestamp = int(datetime.timestamp(faker.date_time()))
+        dt_object = datetime.fromtimestamp(timestamp)
+        formatted_date = dt_object.strftime("%Y-%m-%d %H:%M:%S")
+
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
         value = self.generate_random_measurement()
         msg = {
             "timestamp": timestamp,
             "sensor_id": self.sensor_id,
             "sensor_type": self.sensor_type,
-            "value": value
+            "value": value,
+            "unit": self.unit
         }
-        return msg
+        print(f"Message created '{msg}'.")
+        return json.dumps(msg)
 
 
