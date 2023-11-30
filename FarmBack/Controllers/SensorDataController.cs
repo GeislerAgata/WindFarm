a8ab4b58aafbd84e935883816081a084078cb63c
Agata Geisler <geisler.agata@wp.pl>
2023-11-30 20:02:42 +0100

    All API with added.

---
+++ FarmBack/Controllers/SensorDataController.cs
@@ -0,0 +1,99 @@
+﻿using FarmBack.DTO;
+using FarmBack.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarmBack.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SensorDataController : ControllerBase
+{
+    private readonly ISensorDataRepository _repository;
+    //private readonly SensorDataRepository _repository;
+
+    public SensorDataController(ISensorDataRepository repository)
+    //public SensorDataController()
+    {
+        //_repository = new SensorDataRepository("mongodb://localhost:27017", "windfarm", "windfarm");
+        this._repository = repository;
+    }
+
+    // GET /SensorData
+    [HttpGet]
+    public List<SensorData> GetSensorsData()
+    {
+        return _repository.GetSensorsData();
+    }
+
+    // GET /SensorData/id/{sensorId}
+    [HttpGet("{sensorId}")]
+    public ActionResult<List<SensorData>> GetSensorData(int sensorId)
+    {
+        var data = _repository.GetSensorData(sensorId);
+        if (!data.Any())
+        {
+            return NotFound();
+        }
+        return data;
+    }
+    
+    // GET /SensorData/time/{timestamp}
+    [HttpGet("time/{timestamp}")]
+    public ActionResult<List<SensorData>> GetSensorsDataByTime(DateTime timestamp)
+    {
+        var data = _repository.GetSensorsDataByTime(timestamp);
+        if (!data.Any())
+        {
+            return NotFound();
+        }
+        return data;
+    }
+
+    // GET /SensorData/type/{sensorType}
+    [HttpGet("type/{sensorType}")]
+    public ActionResult<List<SensorData>> GetSensorsDataBySensorType(string sensorType)
+    {
+        var data = _repository.GetSensorsDataBySensorType(sensorType);
+        if (!data.Any())
+        {
+            return NotFound();
+        }
+        return data;
+    }
+
+    // GET /SensorData/sortBy/{sortBy}/trend/{ascending}
+    [HttpGet("sortBy/{sortBy}/trend/{ascending}")]
+    public ActionResult<List<SensorData>> GetSortedSensorData(string sortBy, bool ascending)
+    {
+        var data = _repository.GetSortedSensorData(sortBy, ascending);
+        if (!data.Any())
+        {
+            return NotFound();
+        }
+        return data;
+    }
+
+    // GET /SensorData/id/{sensorId}/time/{timestamp}
+    [HttpGet("id/{sensorId}/time/{timestamp}")]
+    public ActionResult<List<SensorData>> GetSensorDataByIdAndTime(int sensorId, DateTime timestamp)
+    {
+        var data = _repository.GetSensorDataByIdAndTime(sensorId, timestamp);
+        if (!data.Any())
+        {
+            return NotFound();
+        }
+        return data;
+    }
+
+    // GET /SensorData/type/{sensorType}/time/{timestamp}
+    [HttpGet("type/{sensorType}/time/{timestamp}")]
+    public ActionResult<List<SensorData>> GetSensorDataByTypeAndTime(string sensorType, DateTime timestamp)
+    {
+        var data = _repository.GetSensorDataByTypeAndTime(sensorType, timestamp);
+        if (!data.Any())
+        {
+            return NotFound();
+        }
+        return data;
+    }
+}
---
+++ FarmBack/Services/RabbitMQConsumerService.cs
@@ -0,0 +1,65 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+using FarmBack.DTO;
+using Newtonsoft.Json;
+
+namespace FarmBack.Services
+{
+    public class RabbitMQConsumerService : BackgroundService
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private readonly string filePath = "messages.txt";
+        private readonly SensorDataRepository _sensorDataRepository;
+
+        public RabbitMQConsumerService()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+            
+            _sensorDataRepository = new SensorDataRepository("mongodb://localhost:27017", "windfarm", "windfarm");
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var queues = new List<string>
+            {
+                "wind_speed",
+                "temperature",
+                "vibrations",
+                "noise"
+            };
+
+            foreach (var queue in queues)
+            {
+                var consumer = new EventingBasicConsumer(_channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    
+                    var sensorData = JsonConvert.DeserializeObject<SensorData>(message);
+                    _sensorDataRepository.InsertSensorData(sensorData);
+
+                    //File.AppendAllText(filePath, $"Queue: {queue}, Message: {message}\n");
+                };
+
+                _channel.BasicConsume(queue: queue, autoAck: true, consumer: consumer);
+            }
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
